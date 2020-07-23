8621d68cd533e569fe34bf69220ec87671f8dd37
Muhammad Ibrahim Alvi <anusnasim911@gmail.com>
2020-07-23 09:23:40 -0700

    js ass3

---
+++ app.js
@@ -0,0 +1,54 @@
+// CHAPTER 38 TO 42
+// TASK 1
+
+
+// function value(){
+//    var a = prompt("enter first value")
+//    var b= prompt("enter power of value")
+//    var c = Math.pow(a,b)
+//     document.write("the value of A raised to B is "+c)
+// }
+// value();
+
+// TASK 2
+
+// function leap(){
+//     var a = prompt("enter your year")
+//     if(a % 4==0){
+//         document.write(a+" is  a leap year")
+//     }
+//     else if(a % 100 == 0){
+//         document.write(a+" is  a leap year");
+
+//     }
+//     else if(a % 400 == 0){
+//         document.write(a+" is  a leap year");
+
+//     }
+//     else{
+//         document.write(a+" is not a leap year")
+//     }
+// }
+// leap();
+
+
+
+
+// TASK 3
+
+// function triangle(){
+
+// var side1 = prompt("enter side A")
+// var side2 = prompt("enter side B")
+// var side3 = prompt("enter side C")
+// var s = (side1 + side2 + side3) / 2;
+// var areaValue = Math.sqrt(s * (s - side1) * (s - side2) * (s - side3))
+// document.write("the area of triangle is "+areaValue)
+
+// }
+
+// triangle();
+
+
+// TASK 4
+
